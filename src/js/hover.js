e05a91243b654a3ef4442d4a1bd7beaffba81742
Jack Fairfield <Jfairfield@azmag.gov>
2020-03-23 11:50:29 -0700

    update hover again.

--- src/js/hover.js
+++ src/js/hover.js
@@ -12,12 +12,18 @@ define([
             // check if a feature is returned from the Layer
             // do something with the result graphic
             const filteredGfx = response.results.filter(function(result) {
-                return result.graphic.layer != "tracks";
+                console.log(result.graphic.layer.id);
+
+                return !['gray-base-layer', 'tracts'].includes(result.graphic.layer.id);
             })
 
+            var tt = $(".iconTooltip");
+            tt.hide();
             if (filteredGfx.length > 0) {
                 let resultGraphic = filteredGfx[0].graphic;
                 var tooltipHtml = resultGraphic.attributes.Name;
+                console.log(resultGraphic);
+
 
                 var tt = $(".iconTooltip");
                 var text = $(".iconTooltiptext");
@@ -27,8 +33,6 @@ define([
                     left: response.screenPoint.x + 40,
                     top: response.screenPoint.y - 10
                 });
-            } else {
-                tt.hide();
             }
         });
 

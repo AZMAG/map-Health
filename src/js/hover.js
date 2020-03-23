e7d50deb9952b9271db19ea0c1a044335a15134d
Jack Fairfield <Jfairfield@azmag.gov>
2020-03-23 11:46:18 -0700

    update hover

--- src/js/hover.js
+++ src/js/hover.js
@@ -1,7 +1,7 @@
 define([
     "mag/config",
     "mag/map",
-], function (config, {
+], function(config, {
     map,
     view
 }) {
@@ -9,31 +9,30 @@ define([
     view.on("pointer-move", function(event) {
         $(".iconTooltip").hide();
         view.hitTest(event).then(function(response) {
-          // check if a feature is returned from the Layer
-          // do something with the result graphic
-          const resultGraphic = response.results.filter(function(result) {
-            return result.graphic.layer != "tracks";
-          })[0].graphic;
+            // check if a feature is returned from the Layer
+            // do something with the result graphic
+            const filteredGfx = response.results.filter(function(result) {
+                return result.graphic.layer != "tracks";
+            })
 
-          var tooltipHtml = resultGraphic.attributes.Name;
-          console.log(tooltipHtml);
+            if (filteredGfx.length > 0) {
+                let resultGraphic = filteredGfx[0].graphic;
+                var tooltipHtml = resultGraphic.attributes.Name;
 
-            var tt = $(".iconTooltip");
-            var text = $(".iconTooltiptext");
-            text.html(tooltipHtml);
-            tt.css({
-                display: "block",
-                left: response.screenPoint.x + 40,
-                top: response.screenPoint.y - 10
-            });
-
-
+                var tt = $(".iconTooltip");
+                var text = $(".iconTooltiptext");
+                text.html(tooltipHtml);
+                tt.css({
+                    display: "block",
+                    left: response.screenPoint.x + 40,
+                    top: response.screenPoint.y - 10
+                });
+            } else {
+                tt.hide();
+            }
         });
 
-
-
-
-      });
+    });
 
 
 

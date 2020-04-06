5d0cf715dc4258f054c79faef62e6bfd64ed04d0
Jack Fairfield <Jfairfield@azmag.gov>
2020-04-06 09:04:26 -0700

    update config items

--- src/js/config.js
+++ src/js/config.js
@@ -1,17 +1,22 @@
-define([], function() {
-
-    var streetsURL = "https://server.arcgisonline.com/arcgis/rest/services/Reference/World_Transportation/MapServer";
+define([], function () {
+    var streetsURL =
+        "https://server.arcgisonline.com/arcgis/rest/services/Reference/World_Transportation/MapServer";
 
     return {
         // mainUrl: "https://services1.arcgis.com/mpVYz37anSdrK4d8/arcgis/rest/services/AZLicensedFacilities/FeatureServer/",
-        mainUrl: "https://geo.azmag.gov/arcgis/rest/services/maps/HealthData/MapServer/",
-        feedbackUrl: "https://geo.azmag.gov/services/HealthcareFeedback/Feedback/SendFeedback",
+        mainUrl:
+            "https://geo.azmag.gov/arcgis/rest/services/maps/HealthData/MapServer/",
+        feedbackUrl:
+            "https://geo.azmag.gov/services/HealthcareFeedback/Feedback/SendFeedback",
 
-        covidLayerURL: "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases_US/FeatureServer/0/query?where=Province_State+%3D+%27Arizona%27&outFields=*&f=json",
+        covidLayerURL:
+            "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases_US/FeatureServer/0/query?where=Province_State+%3D+%27Arizona%27&outFields=*&f=json",
 
-        healthLayerURL: "https://geo.azmag.gov/arcgis/rest/services/maps/HealthData/MapServer/0",
-        
-        historicalCovidDataURL: "https://geo.azmag.gov/arcgis/rest/services/Hosted/Covid_19_NY_Times_Historical/FeatureServer/0",
+        healthLayerURL:
+            "https://geo.azmag.gov/arcgis/rest/services/maps/HealthData/MapServer/0",
+
+        historicalCovidDataURL:
+            "https://geo.azmag.gov/arcgis/rest/services/Hosted/Covid_19_NY_Times_Historical/FeatureServer/0",
 
         queryLayerIndex: 6,
         demographicsLayerIndex: 7,
@@ -21,162 +26,176 @@ define([], function() {
             xmax: -11862672.960303124,
             ymax: 4346362.140125967,
             spatialReference: {
-                wkid: 102100
-            }
+                wkid: 102100,
+            },
         },
 
         version: "v1.0.0 | 2020-04-01",
         copyright: "2020",
 
         countyLookup: {
-            '001': 'Apache',
-            '003': 'Cochise',
-            '005': 'Coconino',
-            '007': 'Gila',
-            '009': 'Graham',
-            '011': 'Greenlee',
-            '012': 'La Paz',
-            '013': 'Maricopa',
-            '015': 'Mohave',
-            '017': 'Navajo',
-            '019': 'Pima',
-            '021': 'Pinal',
-            '023': 'Santa Cruz',
-            '025': 'Yavapai',
-            '027': 'Yuma'
+            "001": "Apache",
+            "003": "Cochise",
+            "005": "Coconino",
+            "007": "Gila",
+            "009": "Graham",
+            "011": "Greenlee",
+            "012": "La Paz",
+            "013": "Maricopa",
+            "015": "Mohave",
+            "017": "Navajo",
+            "019": "Pima",
+            "021": "Pinal",
+            "023": "Santa Cruz",
+            "025": "Yavapai",
+            "027": "Yuma",
         },
 
         breaks: {
-            "TOTAL_POP": [0, 2879, 4487, 6170, 8907, 17951],
-            "Totoal_Pop_Under_Poverty": [0, 440, 889, 1473, 2331, 3962]
+            TOTAL_POP: [0, 2879, 4487, 6170, 8907, 17951],
+            Totoal_Pop_Under_Poverty: [0, 440, 889, 1473, 2331, 3962],
         },
 
-        layers: [{
+        layers: [
+            {
                 type: "tile",
                 url: streetsURL,
                 title: "Streets",
                 id: "Streets",
                 visible: true,
-                showToc: false
+                showToc: false,
             },
             {
-                type: 'feature',
-                title: 'Residential Facility',
-                id: 'Residential_Facility',
-                definition: 'Facilities intended for long term assistance.',
+                type: "feature",
+                title: "Residential Facility",
+                id: "Residential_Facility",
+                definition: "Facilities intended for long term assistance.",
                 visible: false,
                 scalable: true,
                 showToc: true,
                 index: 4,
-                uvr: [{
-                    value: 'AL Center',
-                    label: 'Assisted Living Center',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/AL Center.svg"
-                    }
-                }, {
-                    value: 'AL Home',
-                    label: 'Assisted Living Home',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/AL Home.svg"
-                    }
-                }]
+                uvr: [
+                    {
+                        value: "AL CENTER",
+                        label: "Assisted Living Center",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/AL Center.svg",
+                        },
+                    },
+                    {
+                        value: "AL HOME",
+                        label: "Assisted Living Home",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/AL Home.svg",
+                        },
+                    },
+                ],
             },
             {
-                type: 'feature',
-                title: 'Long-Term Care Facility',
-                id: 'Long_Term_Care_Facility',
-                definition: 'Nursing Homes',
+                type: "feature",
+                title: "Long-Term Care Facility",
+                id: "Long_Term_Care_Facility",
+                definition: "Nursing Homes",
                 visible: false,
                 scalable: true,
                 showToc: true,
                 index: 2,
-                uvr: [{
-                    value: 'NH',
-                    label: 'Nursing Home',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/NH_1.svg"
-                    }
-                }]
+                uvr: [
+                    {
+                        value: "NH",
+                        label: "Nursing Home",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/NH_1.svg",
+                        },
+                    },
+                ],
             },
             {
-                type: 'feature',
-                title: 'Diagnostic Facilities',
-                id: 'Laboratory',
-                definition: 'Laboratories and other medical testing facilities.',
+                type: "feature",
+                title: "Diagnostic Facilities",
+                id: "Laboratory",
+                definition:
+                    "Laboratories and other medical testing facilities.",
                 visible: false,
                 scalable: true,
                 showToc: true,
                 index: 5,
-                uvr: [{
-                    value: 'Laboratory',
-                    label: 'Testing Facility',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/Laboratory.svg"
-                    }
-                }]
+                uvr: [
+                    {
+                        value: "LABORATORY",
+                        label: "Testing Facility",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/Laboratory.svg",
+                        },
+                    },
+                ],
             },
             {
-                type: 'feature',
-                title: 'Medical Facility',
-                id: 'Medical_Facility',
+                type: "feature",
+                title: "Medical Facility",
+                id: "Medical_Facility",
                 visible: true,
                 showToc: true,
-                definition: 'A variety of outpatient treatment facilities',
+                definition: "A variety of outpatient treatment facilities",
                 index: 3,
-                uvr: [{
-                    value: 'MED - Other',
-                    label: 'Other Medical Facility',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/MED.svg"
-                    }
-                }, {
-                    value: 'OSC',
-                    label: 'Outpatient Surgery Center',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/OSC.svg"
-                    }
-                }, {
-                    value: 'OTC',
-                    label: 'Outpatient Treatment Center',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/OTC.svg"
-                    }
-                },
-                // {
-                //     value: 'BH',
-                //     label: 'Behavioral Health Facility',
-                //     symbol: {
-                //         type: 'picture-marker',
-                //         url: "icons/BH.svg"
-                //     }
-                // }
-            ]
+                uvr: [
+                    {
+                        value: "MED - Other",
+                        label: "Other Medical Facility",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/MED.svg",
+                        },
+                    },
+                    {
+                        value: "OSC",
+                        label: "Outpatient Surgery Center",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/OSC.svg",
+                        },
+                    },
+                    {
+                        value: "OTC",
+                        label: "Outpatient Treatment Center",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/OTC.svg",
+                        },
+                    },
+                    // {
+                    //     value: 'BH',
+                    //     label: 'Behavioral Health Facility',
+                    //     symbol: {
+                    //         type: 'picture-marker',
+                    //         url: "icons/BH.svg"
+                    //     }
+                    // }
+                ],
             },
             {
-                type: 'feature',
-                title: 'Hospital',
-                id: 'Hospital',
+                type: "feature",
+                title: "Hospital",
+                id: "Hospital",
                 visible: true,
                 scalable: true,
                 showToc: true,
                 index: 1,
-                uvr: [{
-                    value: 'HOSPITAL',
-                    label: 'Hospital',
-                    symbol: {
-                        type: 'picture-marker',
-                        url: "icons/HOSPITAL.svg"
-                    }
-                }]
-            }
-        ]
-    }
+                uvr: [
+                    {
+                        value: "HOSPITAL",
+                        label: "Hospital",
+                        symbol: {
+                            type: "picture-marker",
+                            url: "icons/HOSPITAL.svg",
+                        },
+                    },
+                ],
+            },
+        ],
+    };
 });
